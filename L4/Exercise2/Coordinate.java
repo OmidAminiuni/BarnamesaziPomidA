90f13b1d31da10161f0fffb4f56e84ab27790335
OmidAminiuni <102870976+OmidAminiuni@users.noreply.github.com>
2022-04-02 22:05:32 +0430

    Create Coordinate.java

---
+++ L4/Exercise2/Coordinate.java
@@ -0,0 +1,9 @@
+package Exercise2;
+public class Coordinate<first,second,third> implements Cloneable {
+    public first name;
+    public second age;
+    public third Id;
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
